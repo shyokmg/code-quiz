40a0ab8c9a2f8ba1e3597a05439c2b318d6b3fb9
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-09-09 18:51:09 +1000

    Updated script file to record scores in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,38 @@
 // Get elements
-var startButton = document.getElementById("start-button");
 var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-button");
 var startEl = document.querySelector(".start-container");
 var quizEl = document.querySelector(".quiz-container");
 var scoreEl = document.querySelector(".finalscore-container");
 var bottomResult = document.querySelector(".result");
 var questionEl = document.querySelector(".questions");
 var choicesChild = document.getElementById("choices");
+var scoreResult = document.getElementById("score-result");
+
+var scoreForm = document.querySelector("#score-form");
+var initialsInput = document.querySelector("#initials-text");
+var backButton = document.querySelector("#back");
+var scoreList = document.querySelector("#score-list");
+
+var highscores = [];
+
 
 //  set variables
 var secondsLeft = 0;
 var currentQuestion;
 var index = 0;
 
-timerEl.textContent = "Time: " + secondsLeft;
 
+var timerInterval;
 // Timer functions
 function setTime() {
     // Set timer to 60 seconds
-    secondsLeft = 60;
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timerEl.textContent = "Time: " + secondsLeft;
-        if (secondsLeft === 0 || secondsLeft < 0) {
-            clearInterval(timerInterval);
-            // End game 
+    secondsLeft = 75;
+    timerInterval = setInterval(function () {
+        if (secondsLeft > 0){
+            timerEl.textContent = "Time: " + secondsLeft;
+            secondsLeft--;
+        } else {
             gameOver();
         }
     }, 1000);
@@ -33,10 +41,14 @@ function setTime() {
 
 // Function when start button is pressed
 startButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    // start timer
     setTime();
-    nextQuestion();
+    // set questions
+    // show question container and hide the start container
     startEl.setAttribute("data-state", "hidden");
     quizEl.setAttribute("data-state", "visible");
+    nextQuestion();
 });
 
 // Function that identifies which answer is clicked in the question
@@ -45,29 +57,29 @@ quizEl.addEventListener("click", function (event) {
 
     if (element.matches("button")) {
 
+        // get data attr from buttons
         var answer = element.dataset.answer;
+
+        // increment index number everytime a choice is clicked
         index++;
         if (answer === "correct") {
-            // do something, move to next question and set bottom result to correct
+            // do something, set bottom result to correct
             bottomResult.textContent = "Correct!"
 
             // move to next question.
             nextQuestion();
         } else {
-            // do something, substract -15 to timer and set bottom result to wrong
+            // set bottom result to wrong
             bottomResult.textContent = "Wrong!"
-            if (secondsLeft > 0) {
-                // subtract 15 sec from timer
-                secondsLeft -= 15;
-                // move to next question
-                nextQuestion();
-            } else {
-                secondsLeft = 0;
-            }
+            // subtract 15 sec from timer
+            secondsLeft = secondsLeft - 15;
+            // move to next question
+            nextQuestion();
         }
     }
 })
 
+// Array for the available questions
 var questionArr = [
     "question1",
     "question2",
@@ -81,7 +93,7 @@ var questionArr = [
     "question10"
 ];
 
-
+//Object with all available answers
 var answerObj = {
     choices: [
         ["a", "b", "c", "d"], 
@@ -96,21 +108,31 @@ var answerObj = {
         ["a", "b", "c", "d"]],
     cheatSheet: [
         ["correct", "wrong", "wrong", "wrong"], 
-        ["wrong", "correct", "wrong", "wrong"], 
-        ["wrong", "wrong", "wrong", "correct"], 
         ["correct", "wrong", "wrong", "wrong"], 
-        ["wrong", "wrong", "correct", "wrong"], 
-        ["wrong", "correct", "wrong", "wrong"],
-        ["wrong", "correct", "wrong", "wrong"], 
         ["correct", "wrong", "wrong", "wrong"], 
-        ["wrong", "wrong", "correct", "wrong"], 
-        ["wrong", "wrong", "wrong", "correct"]
+        ["correct", "wrong", "wrong", "wrong"], 
+        ["correct", "wrong", "wrong", "wrong"], 
+        ["correct", "wrong", "wrong", "wrong"], 
+        ["correct", "wrong", "wrong", "wrong"], 
+        ["correct", "wrong", "wrong", "wrong"], 
+        ["correct", "wrong", "wrong", "wrong"], 
+        ["correct", "wrong", "wrong", "wrong"]
+        // ["correct", "wrong", "wrong", "wrong"], 
+        // ["wrong", "correct", "wrong", "wrong"], 
+        // ["wrong", "wrong", "wrong", "correct"], 
+        // ["correct", "wrong", "wrong", "wrong"], 
+        // ["wrong", "wrong", "correct", "wrong"], 
+        // ["wrong", "correct", "wrong", "wrong"],
+        // ["wrong", "correct", "wrong", "wrong"], 
+        // ["correct", "wrong", "wrong", "wrong"], 
+        // ["wrong", "wrong", "correct", "wrong"], 
+        // ["wrong", "wrong", "wrong", "correct"]
     ],
 };
 
 
 
-
+// put questions and answers into object
 var quizObj = {
     questions: questionArr,
     answers: answerObj
@@ -118,18 +140,21 @@ var quizObj = {
 
 
 function nextQuestion() {
+    // checks if there are questions left
     if (index + 1 <= questionArr.length) {
+        // get current question based on current index
         currentQuestion = quizObj.questions[index];
+        // populate question container with current question
         questionEl.textContent = currentQuestion;
 
         var choicesButtons = quizObj.answers.choices[index];
         var choicesAnswers = quizObj.answers.cheatSheet[index];
         for (var i=0; i< choicesButtons.length; i++){
+            // Adds the choices to buttons
             choicesChild.children[i].children[0].textContent = choicesButtons[i];
-            choicesChild.children[i].children[0].setAttribute("data-answer", choicesAnswers[i])
+            // sets the data-answer attr to the buttons to determine which answer is correct/wrong
+            choicesChild.children[i].children[0].setAttribute("data-answer", choicesAnswers[i]);
         }
-
-
     } else {
         gameOver();
     }
@@ -137,6 +162,55 @@ function nextQuestion() {
 
 // function to end game and show score
 function gameOver() {
+    clearInterval(timerInterval);
+    if(secondsLeft <= 0){
+        secondsLeft = 0;
+    }
+    timerEl.textContent = "Time: " + secondsLeft;
+    scoreResult.textContent = secondsLeft;
     quizEl.setAttribute("data-state", "hidden");
     scoreEl.setAttribute("data-state", "visible");
 }
+
+function renderScores() {
+    for (var i = 0; i < highscores.length; i++){
+        var highscore = highscores[i];
+        var li = document.createElement("li");
+        li.textContent = highscore;
+        li.setAttribute("data-index", i);
+        scoreList.appendChild(li);
+    }
+}
+
+function init() {
+    var storedScores = JSON.parse(localStorage.getItem("highscores"));
+    // secondsLeft = 0;
+    timerEl.textContent = "Time: " + secondsLeft;
+    if (storedScores !== null){
+        highscores = storedScores;
+    }
+    // renderScores();
+}
+
+function storeScores() {
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+
+scoreForm.addEventListener("submit", function(event){
+    // event.preventDefault();
+    window.location.href = "highscores.html";
+    var initialsText = initialsInput.value.trim();
+    var timeLeft = secondsLeft;
+    var scoreEntry = initialsText + " - " + timeLeft;
+
+    if (initialsText === ""){
+        return;
+    }
+    highscores.push(scoreEntry);
+    initialsInput.value = "";
+
+    storeScores();
+    renderScores();
+})
+
+init();
